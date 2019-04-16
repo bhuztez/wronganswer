a80747a61bcdd25a9f3f04df5a23e0f1233d9bcf
bhuztez <bhuztez@gmail.com>
2019-04-17 00:47:44 +0800

    process dependency links

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
           'leetcode-cn.com = wronganswer.client.LC:LeetcodeClient',
         ]},
 
-    dependency_links=['git+git://github.com/bhuztez/miasma@5f3ccbbf060ede575ff1d866fede7ec751e8d421#egg=miasma'],
     install_requires = ['miasma'],
+    dependency_links=['git+git://github.com/bhuztez/miasma@5f3ccbbf060ede575ff1d866fede7ec751e8d421#egg=miasma'],
     zip_safe = False,
 )
