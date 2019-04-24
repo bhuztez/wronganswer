c83d9cb9dfc514feab5fd246bff1dce45355d6f6
bhuztez <bhuztez@gmail.com>
2019-04-24 13:13:18 +0800

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'wronganswer',
-    version = '0.1.0',
+    version = '0.1.1',
 
     url = 'https://github.com/bhuztez/wronganswer',
     description = 'online judge clients',
